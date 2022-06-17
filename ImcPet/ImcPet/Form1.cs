a3408837e9cfd5f5d32f3908423d1fb012b5aa50
renanll <renan_ll@hotmail.com>
2022-06-17 17:45:26 -0300

    Feat:arrumando alguns botoes

--- ImcPet/ImcPet/Form1.cs
+++ ImcPet/ImcPet/Form1.cs
@@ -19,7 +19,159 @@ namespace ImcPet
 
         private void button1_Click(object sender, EventArgs e)
         {
+            double Altura = 0;
+            double Peso = 0;
+            Altura = double.Parse(TxtAltura.Text);
+            Peso = double.Parse(TxtPeso.Text);
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            TxtName.Clear();
+            TxtPeso.Clear();
+            TxtAltura.Clear();
+            TxtDescricao.Clear();
+            CmbGenero.Text = "";
+            CmbRaca.Text = "";
+            CmbTipo.Text = "";
+        }
+
+        private void TxtDescricao_TextChanged(object sender, EventArgs e)
+        {
+            /*
+            Dogs
+            Akita
+            Basset hound
+            Bichon frisé
+            Boiadeiro australiano
+            Border collie
+            Boston terrier
+            Boxer
+            Buldogue francês
+            Budogue inglês
+            Bull terrier
+            Cane corso
+            Cavalier king
+            Chihuahua
+            Chow chow
+            cocker spaniel inglês
+            Dachshund
+            Dálmata
+            Doberman
+            Dogo argentino
+            Dogue alemão
+            Fila brasileiro
+            Golden retriver
+            Husky siberiano
+            Jack russell
+            Labrador retriver
+            Lhasa apso
+            Lulu da pomerânia
+            Maltes
+            Maltês
+            Mastiff inglês
+            Mastim tibetano
+            Pastor alemão
+            Pastor australiano
+            Pastor de shetland
+            Pequinês
+            Pinscher
+            Pit bull
+            Poodle
+            Pug
+            Rottweiler
+            Schnauzer
+            Shar - pei
+            Shiba
+            Shih tzu
+            Staffordshire bull
+            Weimaraner
+            Yorkshire
+            
+            Cats
+            Persa
+            Maine Coon
+            Gato de pelo
+            Gato de bengala
+            Siamês
+            Sphynx
+            Ragdoll
+            Munchi
+            Scottish fold
+            Noruegues da floresta
+            Siberiano
+            Savannah
+            Gato de pelo curto
+            Exótico
+            Angorá
+            Himalaio
+            Azul russo
+            Gato comum
+            Sagrado da birmânia
+            Chartreux
+            Abissio
+            Bombaim
+            Kinkalow
+            Birmanês
+            Oriental
+            Ragamuffin
+            Somalli
+            Singapura
+            Curt americano
+            British Longhair
+            Toyger
+            Van turco
+            Manês
+            Donskoy
+            Oriental havana
+            Ocicat
+            Balinês
+            Laperm
+            Cymric
+            Cornish rex
+            Bobtail americano
+            Nebelung
+            Thai
+            Korat
+            Tonquinês
+            Lykoi
+            Peterbald
+            Burmillia
+            American wirehair
+            Oriental longhair
+
+            */
+
+
+
+
+        }
+
     }
-}
+}     
